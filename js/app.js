e9c8d14458c7359386437e339b9bc879b5409b0c
trinh <huy8895@gmail.com>
2022-05-06 09:41:37 +0700

    FIX sai chinh ta

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ function initTopBar(){
 		                  <div class="col-lg-8 col-md-8"> 
 		                      <ul class="top-info text-center text-md-left"> 
 		                          <li><i class="fas fa-map-marker-alt"></i> 
-		                              <p class="info-text">SAODO BLIILDINGS | số 2/46 phố Trường Lâm, Đức Giang, Long Biên, Hà Nội</p> 
+		                              <p class="info-text">SAODO BUILDINGS | số 2/46 phố Trường Lâm, Đức Giang, Long Biên, Hà Nội</p> 
 		                          </li> 
 		                      </ul> 
 		                  </div> 
